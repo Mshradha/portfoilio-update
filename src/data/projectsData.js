889f637a49e429f3df041810d5fe0161753475df
Shradha Mishra <shradhamishra2224@gmail.com>
2024-09-20 19:12:44 +0530

    feat: added new project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,30 +6,51 @@ import adidas_image from "../assets/svg/projects/Tomtop.PNG";
 // import bankman_image from "../assets/svg/projects/be.png";
 import beauty_image2 from "../assets/svg/projects/Hours.PNG"
 import bankmanLanding_image from "../assets/svg/projects/Saphora.PNG";
+import scratch from "../assets/svg/projects/scar.png";
+
 // import expense_image from "../assets/svg/projects/weather.png";
 export const projectsData = [
   {
     id: 1,
+    projectName: "Game by Scratch.mit",
+    projectDesc:
+      "The platform supports multiple sprites, motion animations, and collision detection, allowing users to build dynamic, engaging experiences.",
+    tags: ["React", "JavaScript", "CSS", "HTML"],
+    code: "https://github.com/Mshradha/juspay-assignment",
+    demo: "https://juspay-assignment-cyan.vercel.app/",
+    image: scratch,
+  },
+  {
+    id: 2,
     projectName: "MyHours",
     projectDesc:
       "This is a  clone of website myHours, I have tried to implement the website product page  with login and signup authoristaion",
-    tags: ["Node.js","Express","MongoDB","React","JavaScript", "CSS", "ChakraUI", "HTML"],
+    tags: [
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "React",
+      "JavaScript",
+      "CSS",
+      "ChakraUI",
+      "HTML",
+    ],
     code: "https://github.com/Mshradha/bouncy-donkey-4696",
     demo: "https://myhours.vercel.app/",
     image: beauty_image2,
   },
   {
-    id: 2,
+    id: 3,
     projectName: "Revv",
     projectDesc:
       "This is a mini clone of website Revv, I have tried to implement the website product page  with login and signup authoristaion",
-    tags: ["React","Redux","JavaScript", "CSS", "ChakraUI", "HTML"],
+    tags: ["React", "Redux", "JavaScript", "CSS", "ChakraUI", "HTML"],
     code: "https://github.com/Mshradha/assorted-bulb-9592",
     demo: "https://assorted-bulb-9592.netlify.app/",
     image: beauty_image1,
   },
   {
-    id: 3,
+    id: 4,
     projectName: "Bath & Body Works",
     projectDesc:
       "This is a full-fledged clone of website Bath & Body Works, we have tried to implement every possible feature of the website and provided the full payment cycle with login and signup authoristaion",
@@ -39,18 +60,18 @@ export const projectsData = [
     image: beauty_image,
   },
   {
-    id: 6,
+    id: 7,
     projectName: "Mini E-commerce",
     projectDesc:
       "This is a mini clone of website myntra, I have tried to implement the website product page  with login and signup authoristaion",
-    tags: ["React","JavaScript", "CSS", "ChakraUI", "HTML"],
+    tags: ["React", "JavaScript", "CSS", "ChakraUI", "HTML"],
     code: "https://github.com/masai-course/shradha_fw16_237/tree/master/ProjectReact/ecommerce",
     demo: "https://shradhaecomm.vercel.app/",
     image: mini,
   },
- 
+
   {
-    id: 4,
+    id: 5,
     projectName: "Tomtop",
     projectDesc:
       "It is a  functional html css model of website TomTop just with login functionality, I have implemented css with great precision and the website  responsive.",
@@ -60,7 +81,7 @@ export const projectsData = [
     image: adidas_image,
   },
   {
-    id: 5,
+    id: 6,
     projectName: "Sephora ",
     projectDesc:
       "Shopping application landing page with login and create account options.It is  simple designed with Transitions,  Sliders and videos",
@@ -99,7 +120,6 @@ export const projectsData = [
   //   demo: "https://app.netlify.com/sites/weather-app-balrampaidar/overview",
   //   image: expense_image,
   // },
- 
 ];
 
 // Do not remove any fields.
