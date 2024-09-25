20f0c9c3b340d649ba91fca79dc2c42e0c4fcb45
Shradha Mishra <99673738+Mshradha@users.noreply.github.com>
2024-09-25 12:46:15 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
-  title: "Hi, I'm Shradha",
-  description1: "Full Stack Developer",
+  // title: "Hi, I'm Shradha",
+  // description1: "Full Stack Developer",
   description2:
     "Experienced Software Developer with a strong ability to design and implement efficient solutions . Skilled in front-end and back-end development , with experience in various technologies used to build and maintain web applications. Passionate about creating user-friendly and impactful software .",
   image: 1,
